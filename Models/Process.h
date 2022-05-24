a86cca470653e9f13c3ca6a24d9a2d9320986e8b
enzo <enzocarnevali708@gmail.com.br>
2022-05-23 23:45:04 -0300

    first commit

--- Models/Process.h
+++ Models/Process.h
@@ -4,7 +4,9 @@ typedef struct
 {
     int PID;                    //Process Id, unique to a Process
     int State;                  //Process current state
-    int ExpectedTime;           //Expected time until tasks are completed
     int StartTime;              //Time since the process entered into ready queue
+    int ExpectedTime;           //Expected time until tasks are completed
+    int ExecutedTime;           //Amount of time already executed
+    int ExpectedIOTime;         //Expected time spent on I/O operations
     int Priority;               //Process priority
 } Process;
--- main.c
+++ main.c
@@ -1,5 +1,28 @@
+#include "Models/Process.h"
+#include "Constants/IO.h"
+#include "Constants/States.h"
+
+
+//Process Scheduler 
 int main(int argc, char const *argv[])
 {
-    printf("blablabla");
+
+    //Priority Queues
+    Process priorityQueue1[10];
+    Process priorityQueue2[10];
+    Process priorityQueue3[10];
+    Process priorityQueue4[10];
+    Process priorityQueue5[10];
+
+    //IO Queues
+    Process printerQueue[5];
+    Process diskQueue[5];
+    Process magTapeQueue[5];
+
+    //Scheduler Properties
+    int currentTime = 0;
+
+
+
     return 0;
 }
