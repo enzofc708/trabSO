f1d3a3f0dccd32020e21936c35f33ed008892aab
enzofc708 <47937189+enzofc708@users.noreply.github.com>
2022-05-27 18:26:47 -0300

    Add files via upload

--- Utils/SchedulerUtils.h
+++ Utils/SchedulerUtils.h
@@ -1,6 +1,11 @@
 int getExecTime(Process* p){
     //ToDo: Alterar esse cara pra detectar se tem processo querendo sair de IO
-    if(p->IOStartTime - (p->ExpectedTime - p->RemainingTime) > 0 && 
+    if(
+       p->IOStartTime - (p->ExpectedTime - p->RemainingTime) > 0 && 
+       p->IOStartTime - (p->ExpectedTime - p->RemainingTime) < QUANTUM_TIME){
+           return p->IOStartTime - (p->ExpectedTime - p->RemainingTime);
+    }
+    else if(p->IOStartTime - (p->ExpectedTime - p->RemainingTime) > 0 && 
        p->IOStartTime - (p->ExpectedTime - p->RemainingTime) < QUANTUM_TIME){
            return p->IOStartTime - (p->ExpectedTime - p->RemainingTime);
     }
